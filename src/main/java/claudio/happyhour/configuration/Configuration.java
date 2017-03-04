cdea23f2a3ab35835fc5b612432a0aa317d8086f
Claudio Davi <cdavisouza@gmail.com>
2017-03-04 12:35:32 -0300

    COnfiguration files

---
+++ src/main/java/claudio/happyhour/configuration/Configuration.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claudio.happyhour.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+/**
+ *
+ * @author claudio
+ */
+//@org.springframework.context.annotation.Configuration
+//@EnableWebMvc
+//@ComponentScan(basePackages = "claudio.happyhour.controller")
+//public class Configuration{
+//    
+//    @Bean
+//    public InternalResourceViewResolver resolver() {
+//        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+//        vr.setPrefix("/");
+//        vr.setSuffix(".jsp");
+//        return vr;
+//    }
+//    
+//    
+//}
--- src/main/java/claudio/happyhour/configuration/DataFiller.java
+++ src/main/java/claudio/happyhour/configuration/DataFiller.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package claudio.happyhour;
+package claudio.happyhour.configuration;
 
 import claudio.happyhour.helper.SetIterableConverter;
 import claudio.happyhour.model.Event;
